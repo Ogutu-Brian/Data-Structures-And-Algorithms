9cbd086a9f9b55752589493d89dc235e23f37841
Ogutu Brian <codingbrian58@gmail.com>
2019-02-08 19:15:28 +0300

    Add test successful test for insertion sort

---
+++ sample_algorithms/algorithms.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+def insertion_sort(inpout_list: List)->List:
+    """Sorts a list and returns the sorted list"""
+    pass
--- tests/test_insertionsort.py
+++ tests/test_insertionsort.py
@@ -1,8 +1,12 @@
 import unittest
+from sample_algorithms.algorithms import insertion_sort
 
 
 class TestInsertionSort(unittest.TestCase):
     """Class that tests insertion sort instances"""
 
-    def test_simple_list_sort(self):
-        pass
+    def test_sorted_list(self):
+        """Tests for the sorting of an already sorted list"""
+        sorted_list = [1, 2, 3, 4]
+        result = insertion_sort(sorted_list)
+        self.assertEqual(sorted_list, result)
