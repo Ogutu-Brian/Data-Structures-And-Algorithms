e126514b3211475d4d995ff713d38fa27e521f87
Ogutu Brian <codingbrian58@gmail.com>
2019-02-08 22:10:31 +0300

    Add enough code to pass unsorted list

--- sample_algorithms/algorithms.py
+++ sample_algorithms/algorithms.py
@@ -3,14 +3,14 @@ from typing import List
 
 def insertion_sort(input_list: List)->None:
     """Sorts a list and returns the sorted in ascending order"""
-    i = 1
+    i = 0
     list_length = len(input_list)-1
-    while(i < list_length):
-        j = i
-        while(j < list_length):
-            if(input_list[j] < input_list[j-1]):
-                temp = input_list[j]
-                input_list[j] = input_list[j-1]
-                input_list[j-1] = temp
+    while(i <= list_length):
+        j = 0
+        while(j <= list_length):
+            if(input_list[i] < input_list[j]):
+                temp = input_list[i]
+                input_list[i] = input_list[j]
+                input_list[j] = temp
             j += 1
         i += 1
