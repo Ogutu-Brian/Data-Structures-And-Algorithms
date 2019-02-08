8188f62f8587c19bda94be636512325cd3d7635f
Ogutu Brian <codingbrian58@gmail.com>
2019-02-08 19:42:37 +0300

    Add enough code to pass test

--- sample_algorithms/algorithms.py
+++ sample_algorithms/algorithms.py
@@ -1,6 +1,20 @@
-from typing import List
+from typing import List, Tuple
 
 
-def insertion_sort(inpout_list: List)->List:
+def swap_values(a, b)->Tuple:
+    """Swaps input values"""
+    return b, a
+
+
+def insertion_sort(input_list: List)->List:
     """Sorts a list and returns the sorted list"""
-    pass
+    i = 1
+    for item in input_list:
+        j = i+1
+        while j < len(input_list)-1:
+            if(input_list[j-1] > input_list[j]):
+                new_values = swap_values(input_list[j-1], input_list[j])
+                input_list[j-1] = new_values[0]
+                input_list[j] = new_values[1]
+            J += 1
+        i += 1
--- tests/test_insertionsort.py
+++ tests/test_insertionsort.py
@@ -1,9 +1,18 @@
 import unittest
-from sample_algorithms.algorithms import insertion_sort
+from sample_algorithms.algorithms import (insertion_sort, swap_values)
 
 
 class TestInsertionSort(unittest.TestCase):
     """Class that tests insertion sort instances"""
+
+    def test_swap(self):
+        """Tests for swapping of values"""
+        a = 3
+        b = 4
+        result =swap_values(a, b)
+        a=result[0]
+        b=result[1]
+        self.assertEqual((a, b), (4, 3))
 
     def test_sorted_list(self):
         """Tests for the sorting of an already sorted list"""
