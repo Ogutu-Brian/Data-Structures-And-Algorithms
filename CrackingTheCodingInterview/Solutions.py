0fec6caf90b07326f0547cd6318306795d447cc9
Brian Ogutu <codingbrian58@gmail.com>
2020-02-23 10:57:30 +0300

    Solve urlify problem

--- CrackingTheCodingInterview/Solutions.py
+++ CrackingTheCodingInterview/Solutions.py
@@ -30,3 +30,11 @@ def check_permutation(first_string, last_string):
             return False
 
     return True
+
+# URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters,and that you are given the "true" length of the string. (Note: If implementing in Java,please use a character array so that you can perform this operation in place.)
+# EXAMPLE
+# Input: "Mr John Smith "
+# Output: "Mr%20John%20Smith"
+
+def urlify(input):
+    return input.strip().replace(' ','%20')
