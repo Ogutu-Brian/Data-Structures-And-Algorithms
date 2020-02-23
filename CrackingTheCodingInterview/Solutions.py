23a089fe29004b8e27f75184c744cc51724262ef
Brian Ogutu <codingbrian58@gmail.com>
2020-02-23 11:06:12 +0300

    Uptimize check permutations code

--- CrackingTheCodingInterview/Solutions.py
+++ CrackingTheCodingInterview/Solutions.py
@@ -15,21 +15,13 @@ def is_unique(example_string):
 # Check Permutation: Given two strings,write a method to decide if one is a permutation of the
 # other.
 def check_permutation(first_string, last_string):
-    if(len(first_string) != len(last_string)):
-        return False
-
     first_char_list = list(first_string.lower())
     last_char_list = list(last_string.lower())
-    array_length = len(first_char_list)
 
     first_char_list.sort()
     last_char_list.sort()
 
-    for index in range(array_length):
-        if first_char_list[index] != last_char_list[index]:
-            return False
-
-    return True
+    return first_char_list == last_char_list
 
 # URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters,and that you are given the "true" length of the string. (Note: If implementing in Java,please use a character array so that you can perform this operation in place.)
 # EXAMPLE
