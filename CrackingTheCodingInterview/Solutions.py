3792e5553dd12bcbf1a70f85fddf57d375dab63f
Brian Ogutu <codingbrian58@gmail.com>
2020-02-23 09:31:27 +0300

    Handle the is unique question

---
+++ CrackingTheCodingInterview/Solutions.py
@@ -0,0 +1,11 @@
+# Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+# Hints: #44, #7 7 7, #732
+
+def is_unique(example_string):
+    index = 1
+
+    for character in example_string:
+        if character in example_string[index:]:
+            return False
+        index += 1
+    return True
