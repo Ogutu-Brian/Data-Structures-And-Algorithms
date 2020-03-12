d157ffa6da8b40ab8755af72800c0e2e1cf5e515
Brian Ogutu <codingbrian58@gmail.com>
2020-03-12 08:54:21 +0300

    Remove unnecessary import

--- sample_algorithms/algorithms.py
+++ sample_algorithms/algorithms.py
@@ -4,19 +4,19 @@ Sample Basic Algorithms
 from typing import List
 
 
-from typing import List
-
-
 def insertion_sort(input_array: List) -> List:
     j = 1
     while j <= (len(input_array)-1):
         key = input_array[j]
         i = j - 1
+
         while i >= 0 and input_array[i] > key:
             input_array[i + 1] = input_array[i]
             i -= 1
+
         input_array[i + 1] = key
         j += 1
+
     return input_array
 
 
@@ -37,8 +37,10 @@ def consucetive_integer_checking_algorithm(m: int, n: int) -> int:
     """
     result = max(m, n)
     t = min(m, n)
+
     if t != 0:
         while (m % t != 0 or n % t != 0):
             t -= 1
         result = t
+
     return result
