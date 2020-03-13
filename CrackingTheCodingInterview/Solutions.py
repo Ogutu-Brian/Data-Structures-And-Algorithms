3999440cce8ea0b9ea9570b30774b3a207eeef8b
Brian Ogutu <codingbrian58@gmail.com>
2020-03-13 10:46:32 +0300

    Add solutions to max subarray problem

--- CrackingTheCodingInterview/Solutions.py
+++ CrackingTheCodingInterview/Solutions.py
@@ -109,3 +109,26 @@ def is_balanced(expression):
             stack.append(token)
 
     return True
+
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+# Input: [-2,1,-3,4,-1,2,1,-5,4],
+# Output: 6
+# Explanation: [4,-1,2,1] has the largest sum = 6.
+
+
+def max_sub_array(nums):
+    if len(nums) == 1:
+        return nums[0]
+
+    total = nums[0]
+    best = nums[0]
+
+    for i in nums[1:]:
+        if i > (total + i):
+            total = i
+        else:
+            total += i
+
+        best = max(best, total)
+
+    return best
